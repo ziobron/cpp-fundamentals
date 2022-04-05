77e3cf2ab718ab4598f00adfc4e8dd6ccda3d19f
Kacper Kachel <71440570+Kapibara44@users.noreply.github.com>
2022-04-05 10:53:14 +0200

    Update 01-tab.cpp

--- 02-types/tasks/01-tab.cpp
+++ 02-types/tasks/01-tab.cpp
@@ -5,13 +5,11 @@ constexpr size_t tab_size = 100;
 int main() {
     int tab[tab_size];
 
-    for (size_t i = 0; i < tab_size; ++i) {
-        if(i%2!=0){
-            tab[i] = i;
-        }
+    for (size_t i = 0; i < tab_size; i++) {
+        tab[i] = 2 * i + 1;
     }
 
-    for (size_t i = 0; i < tab_size; ++i) {
+    for (size_t i = 0; i < tab_size; i++) {
         std::cout << tab[i] << "\n";
     }
 
